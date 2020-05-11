382f607df9486a55ce8496f40d6e900c5057a178
Thagoo <lohitgowda56@gmail.com>
2020-05-11 11:40:20 +0530

    fixup

--- compiler.sh
+++ compiler.sh
@@ -18,6 +18,7 @@ curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
         -d text="synced sources successfully"
+rm -rf device/xiaomi/rolex
 git clone -b pbrp-android-9 --depth=1 https://github.com/Thagoo/recovery_device_xiaomi_rolex device/xiaomi/rolex
 export ALLOW_MISSING_DEPENDENCIES=true
 source build/envsetup.sh
@@ -25,8 +26,8 @@ lunch omni_rolex-eng
 rm kernel/xiaomi/msm8917/Android.bp
 make -j$(nproc --all) recoveryimage | tee log.txt
 if ! [ -a out/target/product/rolex/*U*.zip ];then
-curl -F document=@log.txt "https://api.telegram.org/bot${TOKEN}/sendDocument" \
-        -F chat_id=${CID} \
+curl -F document=@log.txt "https://api.telegram.org/bot${tok}/sendDocument" \
+        -F chat_id=${cid} \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" 
            exit 1
