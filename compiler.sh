bc5deb8b56c0401da5f2e8e645048f105feac0d3
Thagoo <lohitgowda56@gmail.com>
2020-06-18 18:03:32 +0530

    gg

--- compiler.sh
+++ compiler.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 
-wget -c  "http://s1.dlserver.info/Movie/The.Matrix.Reloaded.2003/The.Matrix.2.2003.Bluray%20720p.mkv"
+#wget -c  "http://s1.dlserver.info/Movie/The.Matrix.Reloaded.2003/The.Matrix.2.2003.Bluray%20720p.mkv"
 #unzip *.zip
-mkdir mat
-mv The* matr
-mv matr mat
 sudo apt install p7zip-full megatools -y
-7z a matrix.7z mat
-ZIPNAME=matrix.7z
-megaput --username $MEGAU --password $MEGAP $ZIPNAME
+
+megadl "https://mega.nz/#!drhh0aBC!ukoDkQx76xU1CzL2AHh6Lu-8PHQjS98fE87SmHU_9OQ"
+mkdir gone
+mv gg.mp4 gone
+7z a gonegirl.7z gone
+megaput --username $MEGAU --password $MEGAP gonegirl.7z
