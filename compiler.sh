c52270411bc36e96b749fa2b708beab53894ab02
Thagoo <lohitgowda56@gmail.com>
2020-06-20 13:29:18 +0530

    primer

--- compiler.sh
+++ compiler.sh
@@ -4,9 +4,9 @@
 #unzip *.zip
 sudo apt install p7zip-full megatools ffmpeg libavcodec-extra -y
 
-megadl "https://mega.nz/#!drhh0aBC!ukoDkQx76xU1CzL2AHh6Lu-8PHQjS98fE87SmHU_9OQ"
-ffmpeg -i gg.mp4 -vf scale=640:360 gg480.mp4
-mkdir gone
-mv gg480.mp4 gone
-7z a gonegirl.7z gone
-megaput --username $MEGAU --password $MEGAP gonegirl.7z
+megadl "https://mega.nz/#!0mx2SYQT!Q6V8LPW4bh1_yoSFfqQbgmSr8MZaqgXS51j8zKJRjvs"
+ffmpeg -i primer.mp4 -vf scale=640:360 primer480.mp4
+mkdir primer
+mv primer480.mp4 primer
+7z a primer.7z primer
+megaput --username $MEGAU --password $MEGAP primer.7z
