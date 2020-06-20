477e57259b5e93190ca106df9aa8b88b9b5d3d42
Thagoo <lohitgowda56@gmail.com>
2020-06-20 19:52:17 +0530

    triangle

--- compiler.sh
+++ compiler.sh
@@ -4,9 +4,9 @@
 #unzip *.zip
 sudo apt install p7zip-full megatools ffmpeg libavcodec-extra -y
 
-megadl "https://mega.nz/#!0mx2SYQT!Q6V8LPW4bh1_yoSFfqQbgmSr8MZaqgXS51j8zKJRjvs"
-ffmpeg -i primer.mp4 -vf scale=640:360 primer480.mp4
-mkdir primer
-mv primer480.mp4 primer
-7z a primer.7z primer
-megaput --username $MEGAU --password $MEGAP primer.7z
+megadl "https://mega.nz/#!Yj4hyZ4D!QJoo7UreDctJbDup1DIiGRLEl9CFUr4RJs2QTYXPP4U"
+ffmpeg -i triangle.mp4 -vf scale=640:360 triangle480.mp4
+#mkdir triangle
+#mv triangle480.mp4 triangle
+#7z a triangle.7z triangle
+megaput --username $MEGAU --password $MEGAP triangle480.mp4
