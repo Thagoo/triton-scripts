91930532474f822875899fa70d33e29245d3857f
Thagoo <lohitgowda56@gmail.com>
2020-07-06 21:30:52 +0530

    e

--- compiler.sh
+++ compiler.sh
@@ -4,10 +4,10 @@
 #unzip *.zip
 sudo apt install p7zip-full megatools ffmpeg libavcodec-extra -y
 
-megadl "https://mega.nz/#!5yh2wCCI!9QhXF2Y2mwgPYtznIQALWUXXL_-wXr7PgBSjr958Z-0"
-mv Cap* capcivilwar.mp4
-ffmpeg -i capcivilwar.mp4 -vf scale=640:360 capcivilwar360.mp4
+megadl "https://mega.nz/#!F7JzAYQA!1_-3JgyqqIcoCpDPUxeNsRe34umdDBOp7n4XrkzRJ6w"
+mv Ave* aendgame.mkv
+ffmpeg -i aendgame.mkv -vf scale=640:360 aendgame360.mkv
 #mkdir capcivilwar
 #mv capcivilwar480.mp4 capcivilwar
 #7z a capcivilwar.7z capcivilwar
-megaput --username $MEGAU --password $MEGAP capcivilwar360.mp4
+megaput --username $MEGAU --password $MEGAP aendgame360.mkv
