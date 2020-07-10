71e486337f1c389d9e8695b28eaa4ce90d7ade2f
Thagoo <lohitgowda56@gmail.com>
2020-07-10 10:52:08 +0530

    gtasa

--- compiler.sh
+++ compiler.sh
@@ -2,12 +2,15 @@
 
 #wget -c  "http://s1.dlserver.info/Movie/The.Matrix.Reloaded.2003/The.Matrix.2.2003.Bluray%20720p.mkv"
 #unzip *.zip
-sudo apt install p7zip-full megatools ffmpeg libavcodec-extra -y
-
-megadl "https://mega.nz/#!gygWgCLD!VAVpKscp5dZiadC9Plvj_hcRIvk7UChw3T3G4VGEdeI"
-mv *Cap* captfa.mkv
-ffmpeg -i captfa.mkv -vf scale=640:360 captfa360.mkv
+sudo apt install p7zip-full megatools -y
+#ffmpeg libavcodec-extra -y
+wget http://s5.rexdl.com/android/game/GTA-San-Andreas-v2.00-www.ReXdl.com.zip -nv
+mv GTA* gtasa
+7z a gtasa.7z gtasa
+#megadl "https://mega.nz/#!gygWgCLD!VAVpKscp5dZiadC9Plvj_hcRIvk7UChw3T3G4VGEdeI"
+#mv *Cap* captfa.mkv
+#ffmpeg -i captfa.mkv -vf scale=640:360 captfa360.mkv
 #mkdir capcivilwar
 #mv capcivilwar480.mp4 capcivilwar
 #7z a capcivilwar.7z capcivilwar
-megaput --username $MEGAU --password $MEGAP captfa360.mkv
+megaput --username $MEGAU --password $MEGAP gtasa.7z
