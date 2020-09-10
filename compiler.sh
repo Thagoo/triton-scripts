5bf1a5b793be8e2aa700775d1cae4b9d0605091c
Thagoo <lohitgowda56@gmail.com>
2020-09-10 06:58:01 +0000

    fix script

--- compiler.sh
+++ compiler.sh
@@ -1,24 +1,9 @@
 #! /usr/bin/bash
 
 ZIPNAME=Triton-Atmosphere-$(date +"%S-%F")
-
+PATH="/tmp/clang/bin:/tmp/gcc64/bin:/tmp/gcc32/bin:${PATH}"
 make O=out ARCH=arm64 rolex_defconfig
-
-export CC=/tmp/clang/bin/clang
-#export KBUILD_COMPILER_STRING="$(${CC} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
-export CROSS_COMPILE=/tmp/gcc64/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=/tmp/gcc32/bin/arm-linux-androideabi-
-export CLANG_TRIPLE=aarch64-linux-gnu-
-export ARCH=arm64
-export SUBARCH=arm64
-
-PATH="/tmp/clang/bin:/tmp/gcc64/bin:/tmp/gcc32/bin:${PATH}" \
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC=clang \
-		      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE=aarch64-linux-android- \
-                      CROSS_COMPILE_ARM32=arm-linux-androideabi-
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
 
 curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
         -d chat_id="$CID" \
