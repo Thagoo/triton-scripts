b097e37db98dc1177149b404202083892ef4319d
Thagoo <lohitgowda56@gmail.com>
2020-11-09 14:54:17 +0000

    orangefox tiare 4.9 test

--- compiler.sh
+++ compiler.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 apt install git curl sudo -y
-export DIRNAME=out/t*/p*/r*
+export DIRNAME=out/t*/p*/t*
 TIME=$(date +"%S-%F")
 #ZIPNAME=PitchBlack-rolex-${TIME}-UNOFFICIAL.zip
 #sudo apt install -y megatools
@@ -8,7 +8,7 @@ git config --global color.ui false
 git config --global user.name Thagoo
 git config --global user.email "lohitgowda56@gmail.com"
 mkdir twrp && cd twrp
-repo init --depth=1 -q -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-9.0
+repo init --depth=1 -q -u https://gitlab.com/OrangeFox/Manifest.git -b fox_9.0 -q
 repo sync -c -q --force-sync --no-clone-bundle --no-tags -j$(nproc --all) 
 if ! [ -a build/env* ];then
 curl -F document=@sync.txt "https://api.telegram.org/bot${TOKEN}/sendDocument" \
@@ -22,17 +22,19 @@ curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
         -d text="synced sources successfully"
-rm -rf device/xiaomi/rolex
-git clone -b test-wip --depth=1 https://github.com/Thagoo/recovery_device_xiaomi_rolex device/xiaomi/rolex 
+rm -rf device/xiaomi/tiare
+git clone -b android-9.0 --depth=1 https://github.com/Thagoo/twrp_device_xiaomi_tiare device/xiaomi/tiare
 export ALLOW_MISSING_DEPENDENCIES=true
 source build/envsetup.sh
-lunch omni_rolex-eng
-rm kernel/xiaomi/msm8917/Android.bp
-rm -rf vendor/qcom/opensource/commonsys/cryptfs_hw
+lunch omni_tiare-eng
+rm kernel/xiaomi/tiare-4.9/Android.bp
+#rm -rf vendor/qcom/opensource/commonsys/cryptfs_hw
 make -j$(nproc --all) recoveryimage
-cd out/target/product/rolex
+cd out/target/product/tiare
 ls
-curl -F document=@recovery.img "https://api.telegram.org/bot${TOKEN}/sendDocument" \
+ZIP=Orangefox-tiare.zip
+mv Orange*.zip $ZIP
+curl -F document=@$ZIP "https://api.telegram.org/bot${TOKEN}/sendDocument" \
         -F chat_id=${CID} \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" 
